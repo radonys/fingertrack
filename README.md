aad09bf9f6b94c2be787e035cfca21bdb1471542
Yash Srivastava <yashsrivastava3005@gmail.com>
2018-04-14 22:29:33 +0530

    Start

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# fingertrack
-Real-time Finger-Detection using Neural Networks (SSD) on Tensorflow
+## Real-time Finger-Detection using Neural Networks (SSD) on Tensorflow.
+
+## Citation
+
+Victor Dibia, Real-time Hand-Detection using Neural Networks (SSD) on Tensorflow, (2017), GitHub repository, https://github.com/victordibia/handtracking
+```bib
+@misc{Dibia2017,
+  author = {Victor, Dibia},
+  title = {Real-time Hand Tracking Using SSD on Tensorflow },
+  year = {2017},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  howpublished = {\url{https://github.com/victordibia/handtracking}},
+  commit = {b523a27393ea1ee34f31451fad656849915c8f42}
+}
+```
+
+## References
+
+Some related and referenced papers.
+
+Bambach, S., Lee, S., Crandall, D. J., and Yu, C. 2015. “Lending A Hand: Detecting Hands and Recognizing Activities in Complex Egocentric Interactions,” in ICCV, pp. 1949–1957 (available at https://www.cv-foundation.org/openaccess/content_iccv_2015/html/Bambach_Lending_A_Hand_ICCV_2015_paper.html).
+
+Erol, A., Bebis, G., Nicolescu, M., Boyle, R. D., and Twombly, X. 2007. “Vision-based hand pose estimation: A review,” Computer Vision and Image Understanding (108:1–2), pp. 52–73 (doi: 10.1016/j.cviu.2006.10.012).
+
+Liu, W., Anguelov, D., Erhan, D., Szegedy, C., Reed, S., Fu, C. Y., and Berg, A. C. 2016. “SSD: Single shot multibox detector,” in European conference on computer vision (Vol. 9905 LNCS), Springer Cham, pp. 21–37 (doi: 10.1007/978-3-319-46448-0_2).
+
+Betancourt, A., Morerio, P., Regazzoni, C. S., and Rauterberg, M. 2015. “The Evolution of First Person Vision Methods: A Survey,” IEEE Transactions on Circuits and Systems for Video Technology (25:5), pp. 744–760 (doi: 10.1109/TCSVT.2015.2409731)
---
+++ detect_single_threaded.py
@@ -0,0 +1,149 @@
+from utils import detector_utils as detector_utils
+import cv2
+import tensorflow as tf
+import datetime
+import argparse
+import numpy as np
+import math
+
+cap = cv2.VideoCapture(0)
+detection_graph, sess = detector_utils.load_inference_graph()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sth', '--scorethreshold', dest='score_thresh', type=float,
+                        default=0.2, help='Score threshold for displaying bounding boxes')
+    parser.add_argument('-fps', '--fps', dest='fps', type=int,
+                        default=1, help='Show FPS on detection/display visualization')
+    parser.add_argument('-src', '--source', dest='video_source',
+                        default=0, help='Device index of the camera.')
+    parser.add_argument('-wd', '--width', dest='width', type=int,
+                        default=320, help='Width of the frames in the video stream.')
+    parser.add_argument('-ht', '--height', dest='height', type=int,
+                        default=180, help='Height of the frames in the video stream.')
+    parser.add_argument('-ds', '--display', dest='display', type=int,
+                        default=1, help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
+                        default=4, help='Number of workers.')
+    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
+                        default=5, help='Size of the queue.')
+    args = parser.parse_args()
+
+    cap = cv2.VideoCapture(args.video_source)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.height)
+
+    start_time = datetime.datetime.now()
+    num_frames = 0
+    im_width, im_height = (cap.get(3), cap.get(4))
+    # max number of hands we want to detect/track
+    num_hands_detect = 1
+
+    while True:
+
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        ret, image_np = cap.read()
+        # image_np = cv2.flip(image_np, 1)
+        try:
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+        except:
+            print("Error converting to RGB")
+
+        # actual detection
+        boxes, scores = detector_utils.detect_objects(
+            image_np, detection_graph, sess)
+
+        # draw bounding boxes
+        crop_img = detector_utils.draw_box_on_image(
+            num_hands_detect, args.score_thresh, scores, boxes, im_width, im_height, image_np)
+
+        grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+        value = (35, 35)
+        blurred = cv2.GaussianBlur(grey, value, 0)
+
+    	# thresholdin: Otsu's Binarization method
+        _, thresh1 = cv2.threshold(blurred, 127, 255,
+                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+        # check OpenCV version to avoid unpacking error
+        (version, _, _) = cv2.__version__.split('.')
+
+        if version == '3':
+            image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
+                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        elif version == '2':
+            contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
+                cv2.CHAIN_APPROX_NONE)
+
+        # find contour with max area
+        cnt = max(contours, key = lambda x: cv2.contourArea(x))
+
+        # create bounding rectangle around the contour (can skip below two lines)
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
+
+        # finding convex hull
+        hull = cv2.convexHull(cnt)
+
+        # drawing contours
+        drawing = np.zeros(crop_img.shape,np.uint8)
+        cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
+        cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
+
+        # finding convex hull
+        hull = cv2.convexHull(cnt, returnPoints=False)
+
+        # finding convexity defects
+        defects = cv2.convexityDefects(cnt, hull)
+        count_defects = 0
+        cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
+        
+        for i in range(defects.shape[0]):
+
+            s,e,f,d = defects[i,0]
+
+            start = tuple(cnt[s][0])
+            end = tuple(cnt[e][0])
+            far = tuple(cnt[f][0])
+
+            # find length of all sides of triangle
+            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+
+            # apply cosine rule here
+            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+
+            # ignore angles > 90 and highlight rest with red dots
+            if angle <= 90:
+                count_defects += 1
+                cv2.circle(crop_img, far, 1, [0,0,255], -1)
+            #dist = cv2.pointPolygonTest(cnt,far,True)
+
+            # draw a line from start to end i.e. the convex points (finger tips)
+            # (can skip this part)
+            cv2.line(crop_img,start, end, [0,255,0], 2)
+            cv2.circle(crop_img,far,5,[0,0,255],-1)
+
+        # Calculate Frames per second (FPS)
+        num_frames += 1
+        elapsed_time = (datetime.datetime.now() -
+                        start_time).total_seconds()
+        fps = num_frames / elapsed_time
+
+        if (args.display > 0):
+            # Display FPS on frame
+            if (args.fps > 0):
+                detector_utils.draw_fps_on_image(
+                    "FPS : " + str(int(fps)), image_np)
+
+            cv2.imshow('Single Threaded Detection', cv2.cvtColor(crop_img, cv2.COLOR_RGB2BGR))
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+        else:
+            print("frames processed: ",  num_frames,
+                  "elapsed time: ", elapsed_time, "fps: ", str(int(fps)))
